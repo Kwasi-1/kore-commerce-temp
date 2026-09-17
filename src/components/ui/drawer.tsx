e69529977097db6e3a7cb3a27dea892706b2e86a
kwasi <affulnana04@gmail.com>
2026-09-17 07:19:34 +0000

    reduce drag handle height

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -70,7 +70,7 @@ const DrawerContent = React.forwardRef<
       <DrawerPrimitive.Title className="sr-only">Mobile Cart Drawer</DrawerPrimitive.Title>
       <div
         className={cn(
-          "mx-auto mt-3 mb-2 shrink-0 h-[5px] w-11 rounded-full bg-muted",
+          "mx-auto mt-3 mb-2 shrink-0 h-[4px] w-11 rounded-full bg-muted",
           dragHandleClassName || drawerDragClassName || drawerDrag,
           typeof drawerDragStyle === "string" && drawerDragStyle
         )}
